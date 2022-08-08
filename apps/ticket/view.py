3612772919c7fb111594135e6e894c163d6db73e
taherkhani <Majid.Taherkhani@digikala.com>
2022-08-08 19:59:45 +0430

    add request limit on api

--- apps/ticket/view.py
+++ apps/ticket/view.py
@@ -18,20 +18,23 @@ def check_proxy(header):
 
 
 @router.post("/new")
-def create_item(token: str, req: schemas.New_ticket, db=Depends(get_db)):
+@limiter.limit("5/minute")
+def create_item(request: Request, token: str, req: schemas.New_ticket, db=Depends(get_db)):
     if not account_crud.is_user(db, token):
         raise HTTPException(status_code=401, detail="for create ticket, login first")
     return crud.create_ticket(db, req, account_crud.get_token(db, token).user_id)
 
 
 @router.get('/all')
-def get_tickets(token: str, db=Depends(get_db)):
+@limiter.limit("5/minute")
+def get_tickets(request: Request, token: str, db=Depends(get_db)):
     if not account_crud.is_user(db, token):
         raise HTTPException(status_code=401, detail="for get tickets, login first")
     return crud.get_ticket(db, account_crud.get_token(db, token).user_id)
 
 
 @router.get('/all/asadmin')
+@limiter.limit("5/minute")
 def get_tickets(request: Request, token: str, db=Depends(get_db)):
     if not check_proxy(request.headers):
         raise HTTPException(status_code=403, detail="invalid hostname")
@@ -41,13 +44,15 @@ def get_tickets(request: Request, token: str, db=Depends(get_db)):
 
 
 @router.post('/answer')
-def answer_ticket(token: str, req: schemas.Answer_ticket, db=Depends(get_db)):
+@limiter.limit("5/minute")
+def answer_ticket(request: Request, token: str, req: schemas.Answer_ticket, db=Depends(get_db)):
     if not account_crud.is_user(db, token):
         raise HTTPException(status_code=401, detail="for answer ticket, login first")
     return crud.answer(db, req, account_crud.get_token(db, token).user_id)
 
 
 @router.post('/answer/asadmin')
+@limiter.limit("5/minute")
 def answer_ticket(request: Request, token: str, req: schemas.Answer_ticket, db=Depends(get_db)):
     if not check_proxy(request.headers):
         raise HTTPException(status_code=403, detail="invalid hostname")
@@ -57,13 +62,15 @@ def answer_ticket(request: Request, token: str, req: schemas.Answer_ticket, db=D
 
 
 @router.post('/set-status')
-def set_status(token: str, req: schemas.New_status, db=Depends(get_db)):
+@limiter.limit("5/minute")
+def set_status(request: Request, token: str, req: schemas.New_status, db=Depends(get_db)):
     if not account_crud.is_user(db, token):
         raise HTTPException(status_code=401, detail="for set ticket status, login first")
     return crud.set_status(db, req, account_crud.get_token(db, token).user_id)
 
 
 @router.post('/set-status/asadmin')
+@limiter.limit("5/minute")
 def set_status(request: Request, token: str, req: schemas.New_status, db=Depends(get_db)):
     if not check_proxy(request.headers):
         raise HTTPException(status_code=403, detail="invalid hostname")
